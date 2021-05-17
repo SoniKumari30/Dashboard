4569f13446db0dd078ec64048ff86728514e96f1
soni kumari <sonikumarisharma30@gmail.com>
2021-05-17 19:34:34 +0530

    my commit

--- src/DashBoard.js
+++ src/DashBoard.js
@@ -6,7 +6,7 @@ class UserProfile extends React.Component{
     constructor()
     {
         super()
-        this.state=
+        this.state=//
         {
             user:[],
             posts:[],
